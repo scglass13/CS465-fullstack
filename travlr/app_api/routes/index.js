c71bb3a5bc378ca6622c737456f00a37bf2f063c
Sean Glass <scglass13@gmail.com>
2024-06-08 16:09:36 -0500

    Module 5 completed baseline

---
+++ travlr/app_api/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express'); // express app
+const router = express.Router(); // router logic
+
+// this is where we import the controllers we will route
+const tripsController = require('../controllers/trips');
+
+// define route for our trips endpoint
+router
+    .route('/trips')
+    .get(tripsController.tripsList); // GET Method routes tripList
+
+// GET method routes tripsFindByCode - requires parameter
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode); 
+
+module.exports = router;
