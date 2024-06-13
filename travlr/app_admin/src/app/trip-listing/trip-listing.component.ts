2ca777928a09621dbf053c9558edcd222836c3db
Sean Glass <scglass13@gmail.com>
2024-06-13 16:54:17 -0500

    fixed images, created trip cards

--- travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
+++ travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { trips } from '../data/trips';
+import { TripCardComponent } from '../trip-card/trip-card.component';
 
 @Component({
   selector: 'app-trip-listing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TripCardComponent],
   templateUrl: './trip-listing.component.html',
   styleUrl: './trip-listing.component.css'
 })
 export class TripListingComponent implements OnInit {
   trips: Array<any> = trips;
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit(): void {
-    
+
   }
 }
