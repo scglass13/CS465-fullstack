c4dda3287f9e83b6bda395a7432fd20809f35c42
Sean Glass <scglass13@gmail.com>
2024-06-14 19:22:44 -0500

    implemented add trip functionality

--- travlr/app_api/controllers/trips.js
+++ travlr/app_api/controllers/trips.js
@@ -1,14 +1,22 @@
 // app_api/controllers/trips.js
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const Trip = require("../models/travlr");
 
-const tripsFilePath = path.join(__dirname, '..', '..', 'public', 'data', 'trips.json');
+const tripsFilePath = path.join(
+  __dirname,
+  "..",
+  "..",
+  "public",
+  "data",
+  "trips.json"
+);
 
 const tripsList = (req, res) => {
-  fs.readFile(tripsFilePath, 'utf8', (err, data) => {
+  fs.readFile(tripsFilePath, "utf8", (err, data) => {
     if (err) {
-      res.status(500).json({ "error": "Could not read file" });
+      res.status(500).json({ error: "Could not read file" });
     } else {
       const trips = JSON.parse(data);
       res.status(200).json(trips);
@@ -18,22 +26,48 @@ const tripsList = (req, res) => {
 
 const tripsFindByCode = (req, res) => {
   const tripCode = req.params.tripCode;
-  fs.readFile(tripsFilePath, 'utf8', (err, data) => {
+  fs.readFile(tripsFilePath, "utf8", (err, data) => {
     if (err) {
-      res.status(500).json({ "error": "Could not read file" });
+      res.status(500).json({ error: "Could not read file" });
     } else {
       const trips = JSON.parse(data);
-      const trip = trips.find(t => t.code === tripCode);
+      const trip = trips.find((t) => t.code === tripCode);
       if (trip) {
         res.status(200).json(trip);
       } else {
-        res.status(404).json({ "message": "Trip not found" });
+        res.status(404).json({ message: "Trip not found" });
       }
     }
   });
 };
 
+// POST: /trips - adds a new trip
+//regardless of outcome, response must include HTML status code and JSON message to requesting client
+const tripsAddTrip = async (req, res) => {
+  const newTrip = new Trip({
+    code: req.body.code,
+    name: req.body.name,
+    length: req.body.length,
+    start: req.body.start,
+    resort: req.body.resort,
+    perPerson: req.body.perPerson,
+    image: req.body.image,
+    description: req.body.description,
+  });
+
+  const q = await newTrip.save();
+
+  if (!q) {
+    //Database returned no data
+    return res.status(400).json(err);
+  } else {
+    // return new trip
+    return res.status(201).json(q);
+  }
+};
+
 module.exports = {
   tripsList,
-  tripsFindByCode
+  tripsFindByCode,
+  tripsAddTrip,
 };
--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -1,17 +1,16 @@
-const express = require('express'); // express app
+const express = require("express"); // express app
 const router = express.Router(); // router logic
 
 // this is where we import the controllers we will route
-const tripsController = require('../controllers/trips');
+const tripsController = require("../controllers/trips");
 
 // define route for our trips endpoint
 router
-    .route('/trips')
-    .get(tripsController.tripsList); // GET Method routes tripList
+  .route("/trips")
+  .get(tripsController.tripsList) // GET Method routes tripList
+  .post(tripsController.tripsAddTrip); // POST method adds a trip
 
 // GET method routes tripsFindByCode - requires parameter
-router
-    .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode); 
+router.route("/trips/:tripCode").get(tripsController.tripsFindByCode);
 
 module.exports = router;
